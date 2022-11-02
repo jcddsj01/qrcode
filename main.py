80ecfb4b413ac7d0d253cc8566cbd6ed6f6d989f
José Carlos <jcddsj01@outlook.com>
2022-11-01 23:46:18 -0300

    Primeiro commit main.py

---
+++ main.py
@@ -0,0 +1,23 @@
+# Instalação de biblioteca
+# pip install qrcode[pil]
+
+# Exemplo: Lista de sites de E-commerce de informática
+import qrcode
+
+imagem_qrcode = qrcode.make('objeto')
+imagem_qrcode.save('nome')
+
+links_sites = {
+  'KaBum': 'https://www.kabum.com.br/',
+  'Pichau': 'https://www.pichau.com.br/',
+  'ChipArt': 'https://www.chipart.com.br/',
+  'Terabyteshop': 'https://www.terabyteshop.com.br/',
+  'MegaMamute': 'https://www.megamamute.com.br/',
+  'Amazon': 'https://www.amazon.com.br/',
+  'Submarino': 'https://www.submarino.com.br/'
+}
+
+for ecommerce in links_sites:
+  link = links_sites[ecommerce]
+  imagem_qrcode = qrcode.make(link)
+  imagem_qrcode.save(f'qrcode_ecommerce_{ecommerce}.png')
