a563e10ef6d6cb29bf38f40d6eca08b0cb4b1fe1
José Carlos <116307228+jcddsj01@users.noreply.github.com>
2022-11-02 00:23:16 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,10 +2,7 @@
 # pip install qrcode[pil]
 
 # Exemplo: Lista de sites de E-commerce de informática
-import qrcode
-
-imagem_qrcode = qrcode.make('objeto')
-imagem_qrcode.save('nome')
+# import qrcode
 
 links_sites = {
   'KaBum': 'https://www.kabum.com.br/',
